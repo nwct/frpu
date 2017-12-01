90c34293b444d724430242611b89321e3a521ec4
chengang <372763861@qq.com>
2017-12-01 23:31:03 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,9 +1,10 @@
 @echo off
 cd %~dp0
 set /p m=请输入标记：
+set /p http=请输入项目网址：
 git init
 git add .
 git commit -m "%m%"
-git remote add origin https://github.com/nwct/frpu.git
+git remote add origin %http%
 git push -u origin master
 pause
